2fd41292f664790461e0c1c596cdf9fe40743493
AngieJC <htk90uggk@outlook.com>
2024-11-25 18:00:08 +0800

    change pprofMemory to PprofMemory

--- mem_pprof.go
+++ mem_pprof.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func pprofMemory(frequent int) {
+func PprofMemory(frequent int) {
 	for i := 0; ; i++ {
 		fm, err := os.OpenFile(fmt.Sprintf("pid%v_mem_%v", os.Getpid(), i), os.O_RDWR|os.O_CREATE, 0644)
 		if err != nil {
